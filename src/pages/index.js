9a2afc08bcca0724012dfada3733db9d09c793f3
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2019-12-16 01:30:26 -0300

    test deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,19 @@
 import React from 'react'
-import { useIntl } from 'gatsby-plugin-intl'
+// import { useIntl } from 'gatsby-plugin-intl'
 
 import Layout from 'layouts'
-// import Image from 'components/image'
-import Box from 'components/box'
+// import Box from 'components/box'
 import SEO from 'components/seo'
-import BlogList from 'components/blogList'
-import * as C from 'styles/common'
-import * as S from 'pages/pageStyles/homeStyled'
+// import BlogList from 'components/blogList'
+// import * as C from 'styles/common'
+// import * as S from 'pages/pageStyles/homeStyled'
 
 const IndexPage = () => {
-  const intl = useIntl()
+  // const intl = useIntl()
   return (
     <Layout>
       <SEO title="Home" />
-      <S.HomeContent>
+      {/* <S.HomeContent>
         <S.PersonalInfo>
           <Box className="mb-2">
             <C.Title>{intl.formatMessage({ id: 'work' })}</C.Title>
@@ -24,13 +23,6 @@ const IndexPage = () => {
             <C.Title>{intl.formatMessage({ id: 'life' })}</C.Title>
             <p>{intl.formatMessage({ id: 'lifeDescription' })}</p>
           </Box>
-          {/*
-          <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-            <Image />
-          </div>
-          <Link to="/page-2/">{intl.formatMessage({ id: 'go_page2' })}</Link>
-          <br />
-          <Link to="/about/">Sobre</Link> */}
         </S.PersonalInfo>
         <S.LastPosts>
           <C.Title>Last Posts</C.Title>
@@ -39,7 +31,7 @@ const IndexPage = () => {
             View posts
           </S.AllPost>
         </S.LastPosts>
-      </S.HomeContent>
+      </S.HomeContent> */}
     </Layout>
   )
 }
