1ba191ef0437957d11080ff2a4179a1342399e06
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2019-12-16 11:57:20 -0300

    change name import tag component

--- src/components/blogItem/index.js
+++ src/components/blogItem/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import Tag from 'components/Tag'
+import Tag from 'components/tag'
 import * as S from './styled'
 
 const BlogItem = ({ info }) => {
