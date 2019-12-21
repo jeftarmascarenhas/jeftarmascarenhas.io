1c2c94a0298bdd8e08131281748d4e368d5da6fd
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2019-12-21 18:10:27 -0300

    remove window

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,14 +1,14 @@
 export const getLocalStore = (key, options = { parser: false }) => {
   if (!key) throw Error('Informe a chave')
-  if (options.parser) return JSON.parse(window.localStorage.getItem(key))
-  return window.localStorage.getItem(key)
+  if (options.parser) return JSON.parse(localStorage.getItem(key))
+  return localStorage.getItem(key)
 }
 
 export const setLocalStore = (key, value, options = { stringify: false }) => {
   if (!key || value === null) throw Error('Informe a chave e valor')
   if (options.stringify) {
-    window.localStorage.setItem(key, JSON.stringify(value))
+    localStorage.setItem(key, JSON.stringify(value))
   } else {
-    window.localStorage.setItem(key, value)
+    localStorage.setItem(key, value)
   }
 }
