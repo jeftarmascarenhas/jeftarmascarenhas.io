a56036c2e9f2bf6187796a5460d248bd14b64511
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2019-12-21 18:38:58 -0300

    fix bug build

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,14 +1,17 @@
+const windowGlobal = typeof window !== `undefined` ? window : null
 export const getLocalStore = (key, options = { parser: false }) => {
+  if (!windowGlobal) return
   if (!key) throw Error('Informe a chave')
-  if (options.parser) return JSON.parse(localStorage.getItem(key))
-  return localStorage.getItem(key)
+  if (options.parser) return JSON.parse(windowGlobal.localStorage.getItem(key))
+  return windowGlobal.localStorage.getItem(key)
 }
 
 export const setLocalStore = (key, value, options = { stringify: false }) => {
+  if (!windowGlobal) return
   if (!key || value === null) throw Error('Informe a chave e valor')
   if (options.stringify) {
-    localStorage.setItem(key, JSON.stringify(value))
+    windowGlobal.localStorage.setItem(key, JSON.stringify(value))
   } else {
-    localStorage.setItem(key, value)
+    windowGlobal.localStorage.setItem(key, value)
   }
 }
