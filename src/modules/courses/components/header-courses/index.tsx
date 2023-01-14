5bbe7001d580144d1013f405d44cb687eb5e7cc1
Jeff Mascarenhas <jeftarmascarenhas@hotmail.com>
2023-01-14 12:07:38 -0300

    refactor: using TitlePage component

--- src/modules/courses/components/header-courses/index.tsx
+++ src/modules/courses/components/header-courses/index.tsx
@@ -1,21 +1,19 @@
 import { Text, VStack } from '@chakra-ui/react'
+import TitlePage from '@global-components/title-page'
 import React from 'react'
+
+const subtitles = [
+  `Aprenda de forma fácil com conteúdo de qualidade e muita prática,
+  entenda como é ser um dev no dia a dia, como estudar e realmente
+  aprender.`,
+  `O conhecimento adquerido nos cursos são testados para válidar se você
+  realmente aprendeu.`
+]
 
 export default function HeaderCourses() {
   return (
     <VStack align="stretch" pr={{ base: '0', xl: '60' }} pb="10">
-      <Text as="h1" fontSize={{ base: '6xl', xl: '7xl' }} fontWeight="semibold">
-        Cursos
-      </Text>
-      <Text as="p" fontSize="larger">
-        Aprenda de forma fácil com conteúdo de qualidade e muita prática,
-        entenda como é ser um dev no dia a dia, como estudar e realmente
-        aprender.
-      </Text>
-      <Text as="p" fontSize="larger">
-        O conhecimento adquerido nos cursos são testados para válidar se você
-        realmente aprendeu.
-      </Text>
+      <TitlePage title="Cursos" subtitles={subtitles} />
     </VStack>
   )
 }
