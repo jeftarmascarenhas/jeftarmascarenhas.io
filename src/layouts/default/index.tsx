39c59361104f4cfcc5b1442b090088057eae9d27
Jeff Mascarenhas <jeftarmascarenhas@hotmail.com>
2023-01-14 12:06:31 -0300

    refactor: removing alert

--- src/layouts/default/index.tsx
+++ src/layouts/default/index.tsx
@@ -1,4 +1,4 @@
-import { Alert, Box } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 import Header from '@global-layouts/default/components/header'
 import { useForceThemeMode } from '@hooks/use-force-theme-mode'
 import { DefaultLayoutProps } from './types'
@@ -7,9 +7,6 @@ export default function Default({ children }: DefaultLayoutProps) {
   useForceThemeMode('light')
   return (
     <Box as="main" pb="8">
-      <Alert status="warning" justifyContent="center" mb="4">
-        <span>Este é meu novo site. Está em desenvolvimento.</span>
-      </Alert>
       <Header />
       {children}
     </Box>
