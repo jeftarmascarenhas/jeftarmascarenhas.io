74e230d814d6a1fdf075e404da69255e04a4d23c
Jeff Mascarenhas <jeftarmascarenhas@hotmail.com>
2023-03-06 20:20:56 -0300

    feat: call to generate xml

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,8 +1,15 @@
 import { getAllPosts } from '@global-libs/api'
 import BlogScreen from '@global-modules/blog/screens/main'
+import { generateSitemap } from '@global-libs/api/sitemap'
+import { Posts } from '@global-entities/post'
 
 export async function getStaticProps() {
-  const posts = getAllPosts()
+  const posts = getAllPosts() as Posts
+
+  if (process.env.NODE_ENV !== 'development') {
+    await generateSitemap(posts)
+  }
+
   return {
     props: {
       posts
